0be62ece2e2bb53fc460c403157a255d584697ea
Juan Gabriel Correa Vásquez <118374698+juangcorrea1@users.noreply.github.com>
2023-06-29 00:10:51 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function agregar() {
+    const conteiner = document.querySelector('#conteiner');
+    const form = document.querySelector('#form');
+    const icon = document.querySelector('#icon');
+    const btnCircle = document.querySelector('#btnCircle');
+    const btnCircle2 = document.querySelector('#btnCircle2');
+    const button = document.querySelector('#button');
+    const text = document.querySelector('#text');
+
+    conteiner.setAttribute('class', 'conteiner');
+    form.setAttribute('class', 'form');
+    icon.setAttribute('class', 'icon');
+    btnCircle.setAttribute('class', 'btnCircle');
+    btnCircle2.setAttribute('class', 'btnCircle');
+    button.setAttribute('class', 'button');
+    text.setAttribute('class', 'text');
+}
